21cb4ba85b08c7a21d2e5f0f9ba61a9c2dd0af42
Brandon McDonald <bamcd0313@gmail.com>
2023-11-09 08:40:16 -0500

    work on dialogue

---
+++ Assets/Scripts/Dialouge/DialogueManager.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEditor.Rendering;
+using System;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI _nameText, _dialogueText;
+    [SerializeField] Image _textBox;
+    [SerializeField] Button _choiceBox1, _choiceBox2;
+    [SerializeField] TextMeshProUGUI _choiceText1, _choiceText2;
+
+    public Action OnPlayerMakeChoice;
+    KeyCode exitKey = KeyCode.Space;
+    int currentDialogue;
+    
+    void Start()
+    {
+        SetAllObjectsToEnabled(false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(exitKey))
+        {
+            currentDialogue++;
+        }
+    }
+    public void StartDialogueSet(string[] dialogueSet, string name)
+    {
+        SetAllObjectsToEnabled(true);
+        SetAllChoicesToEnabled(false);
+        
+        _nameText.text = name + "...";
+        StartCoroutine(ShowDialogues(dialogueSet));
+    }
+    
+    IEnumerator ShowDialogues(string[] dialogueSet)
+    {
+        while (currentDialogue < dialogueSet.Length)
+        {
+            _dialogueText.text = "";
+            string text = dialogueSet[currentDialogue];
+            foreach (char c in text)
+            {
+                
+                _dialogueText.text += c;
+                yield return new WaitForSeconds(0.05f);
+                //if player has skipped to next dialouge
+                if(text != dialogueSet[currentDialogue])
+                {
+                    break;
+                }
+                
+            }
+            if (_dialogueText.text == text)
+            {
+                //has complete the text
+                currentDialogue++;
+            }
+
+        }
+        EndDialogue();
+    }
+    public void ShowChoice(string dialogue, string name, string choice1, string choice2)
+    {
+        SetAllObjectsToEnabled(true);
+        SetAllChoicesToEnabled(true);
+
+        _nameText.text = name + "...";
+        _dialogueText.text = dialogue;
+        _choiceText1.text = choice1;
+        _choiceText2.text = choice2;
+    }
+    public void EndDialogue()
+    {
+       _nameText.text = null;
+        _dialogueText.text = null; 
+        SetAllChoicesToEnabled(false);
+    }
+
+    void SetAllObjectsToEnabled(bool value)
+    {
+        _nameText.enabled = value;
+        _dialogueText.enabled = value;
+        _textBox.enabled = value;
+        _choiceBox1.enabled = value;
+        _choiceBox2.enabled = value;
+        _choiceText1.enabled = value;
+        _choiceText2.enabled = value;
+    }
+
+    void SetAllChoicesToEnabled(bool value)
+    {
+        _choiceBox1.enabled = value;
+        _choiceBox2.enabled = value;
+        _choiceText1.enabled = value;
+        _choiceText2.enabled = value;
+    }
+
+  
+}
+
+
+
+ 
